9a38e228ac681e1d1aaf4b46bab41fafe5bef99d
Arsad <arsadahmed10@gmail.com>
2023-02-15 23:48:17 +0530

    Different types of variable in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
 fun main(args: Array<String>)
 {
     println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+
+    //Mutable Variables or variables which can be reassigned
+    var age=20
+    println("Age is $age")
+
+    age=35
+    println("Age is $age")
+
+    var age2:Int=37
+    println("Age two is $age2")
+
+    //Immutable variables or variables which cannot be reassigned
+    val name="Arshad"
+
+    /*Cannot reassign*/
+    //name="Jasmin"
+    println("Name is $name")
 }
