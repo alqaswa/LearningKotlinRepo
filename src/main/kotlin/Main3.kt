a814d22fdf13d0fd58f9f1b3b2b6f7814d52cebf
Arsad <arsadahmed10@gmail.com>
2023-02-16 19:06:02 +0530

    Taking input from user and use if conditions as expression

---
+++ src/main/kotlin/Main3.kt
@@ -0,0 +1,24 @@
+import java.util.Scanner
+
+fun main(args: Array<String>)
+{
+    println("enter your name")
+    val name= readLine()
+
+    //readLine and Scanner both are same which are used to take input from the user like we use Scanner in java
+    /*val input=Scanner(System.`in`)
+    val name=input.next()*/
+
+    /*This is the benefit we get from kotlin that we can use if conditions as an expression to store result which id return by these conditions
+    and this result can be used for further purpose*/
+    val result=if (name=="Arshad")
+    {
+        "Your name is Arshad"
+    }
+    else
+    {
+        "your name is i don't know"
+    }
+
+    println(result)
+}
